def40ee6d5f85d9849e66b61ad84a06ebdaa8c5c
Thierry-Harvey <thierryh9@gmail.com>
2020-08-27 12:52:45 -0400

    allo

---
+++ source/repos/CustomerPortal/CustomerPortal/Areas/Identity/Data/CustomerPortalUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace CustomerPortal.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the CustomerPortalUser class
+    public class CustomerPortalUser : IdentityUser
+    {
+    }
+}
---
+++ source/repos/CustomerPortal/CustomerPortal/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using CustomerPortal.Areas.Identity.Data;
+using CustomerPortal.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(CustomerPortal.Areas.Identity.IdentityHostingStartup))]
+namespace CustomerPortal.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<CustomerPortalContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("CustomerPortalContextConnection")));
+
+                services.AddDefaultIdentity<CustomerPortalUser>(options => options.SignIn.RequireConfirmedAccount = false)
+                    .AddEntityFrameworkStores<CustomerPortalContext>();
+            });
+        }
+    }
+}
