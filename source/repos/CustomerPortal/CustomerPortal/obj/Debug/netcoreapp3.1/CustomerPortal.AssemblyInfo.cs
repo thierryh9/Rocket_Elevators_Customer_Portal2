5b4fd37b4215a43cfed34aa31ae7af8ac4b01176
Thierry-Harvey <thierryh9@gmail.com>
2020-08-30 17:44:56 -0400

    server

--- source/repos/CustomerPortal/CustomerPortal/obj/Debug/netcoreapp3.1/CustomerPortal.AssemblyInfo.cs
+++ source/repos/CustomerPortal/CustomerPortal/obj/Debug/netcoreapp3.1/CustomerPortal.AssemblyInfo.cs
@@ -12,6 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap4")]
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("c4bd9614-c8bb-4254-a7df-22acab52ddfb")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("CustomerPortal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
